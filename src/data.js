cfd66c7a7209b381eac10325f9b90be4c4340092
Goran <goran.antic@evermedtv.com>
2022-05-29 14:10:08 +0200

    fixed some links

--- src/data.js
+++ src/data.js
@@ -29,55 +29,55 @@ export const data = [
     year: '1998',
     image:
       "https://lh3.googleusercontent.com/eIwxQmmZOei9XmijUXzWsD9UIIPqW4xlzIHSyDq4hsB2KvAHC5vBQzg7Q-nnCdxKog3-5EZUyPsvzJkwMA=w544-h544-l90-rj",
-    url: "https://music.youtube.com/watch?playlist=OLAK5uy_myCWwyHEz9SABGUHaclijR2o9zLXJdE_w",
+    url: "https://music.youtube.com/watch?v=hbe3CQamF8k&list=OLAK5uy_myCWwyHEz9SABGUHaclijR2o9zLXJdE_w",
   },
   {
       artist: 'Air',
       album: 'Moon Safari',
       year: '1998',
       image: 'https://lh3.googleusercontent.com/uRhW0YFArUIhQZw2klqOPb05S_QZadUzKXw6h3eNH1TEG1TV0ooCpPoBYbatsX1xqFG3d2kKCnn_Ot5m=w544-h544-l90-rj',
-      url: 'https://music.youtube.com/watch?playlist=OLAK5uy_muokP2ArFXF_yuj0Qnh_5_QmfFMpwqFj4'
+      url: 'https://music.youtube.com/watch?v=BswuwCn1hBo&list=OLAK5uy_muokP2ArFXF_yuj0Qnh_5_QmfFMpwqFj4'
   },
   {
       artist: 'Fear Factory',
       album: 'Mechanize',
       year: '2010',
       image: 'https://lh3.googleusercontent.com/TpKJrwUnMAG0o2XqKW6wd50TrW4mXo4YY6bKeusodYvqTJIuMcIcAqwCQWrf_t09O7W_btaixdmxF8w=w544-h544-l90-rj',
-      url: 'https://music.youtube.com/watch?playlist=OLAK5uy_n-GolPIASxZGskKVP23eQXBK5cz7nbYUk'
+      url: 'https://music.youtube.com/watch?v=IpxhOQCTPTA&list=OLAK5uy_n-GolPIASxZGskKVP23eQXBK5cz7nbYUk'
   },
   {
       artist: 'Deftones',
       album: 'Koi No Yokan',
       year: '2012',
       image: 'https://lh3.googleusercontent.com/6aBG7KqSZSBz_TyeKkviVbKTfiq1MS3P14-BO3FoZ_j1qW4JbwBLJ3AtAfcVcdMNoiyDmR1RatQUOCMz8A=w544-h544-l90-rj',
-      url: 'https://music.youtube.com/watch?playlist=OLAK5uy_m7pZwI9zdhMaSBX2TkHSIXNi07dH8PxIQ'
+      url: 'https://music.youtube.com/watch?list=OLAK5uy_m7pZwI9zdhMaSBX2TkHSIXNi07dH8PxIQ'
   },
   {
       artist: 'Deftones',
       album: 'Gore',
       year: '2016',
       image: 'https://lh3.googleusercontent.com/XuIuCBgImjW8sca7AopphKQwRUVFEL61i8CfpJ5ECFalb7dqOf-Qv5jPDQJ_aszFNdjiVl7wYq9-xwUrpA=w544-h544-l90-rj',
-      url: 'https://music.youtube.com/watch?playlist=OLAK5uy_nazTzzKNyhdQDiDQLYRe36nUbL05rvze0'
+      url: 'https://music.youtube.com/watch?list=OLAK5uy_nazTzzKNyhdQDiDQLYRe36nUbL05rvze0'
   },
   {
       artist: 'Baroness',
       album: 'The Red Album',
       year: '2007',
       image: 'https://lh3.googleusercontent.com/Yg_WvPGaIlVwm4D4HNk55Ak3vn28_ZOuMtSUNTSCbgL5MfQAaTDv1pN87Lj88DQThMvKgr03RfH2ZyY=w544-h544-l90-rj',
-      url: 'https://music.youtube.com/watch?playlist=OLAK5uy_nfVeYXJAeqDB2qu0g6WUaKU-SDKZ2UbMc'
+      url: 'https://music.youtube.com/watch?list=OLAK5uy_nfVeYXJAeqDB2qu0g6WUaKU-SDKZ2UbMc'
   },
   {
       artist: 'Baroness',
       album: 'Blue Record',
       year: '2009',
       image: 'https://lh3.googleusercontent.com/vtni1o8dHB1dmYJztR8uY4Qt3jQJJ82CMCKNSaCmxULzEWR4XSVaLOsMKA1fC_SiBh8EP7Lg2mIMIsoKJQ=w544-h544-l90-rj',
-      url: 'https://music.youtube.com/watch?playlist=OLAK5uy_lxhs5u7qcpL1DxndwM5UGWWw_50Eiot3E'
+      url: 'https://music.youtube.com/watch?list=OLAK5uy_lxhs5u7qcpL1DxndwM5UGWWw_50Eiot3E'
   },
   {
       artist: 'ZHU',
       album: 'GENERATIONWHY',
       year: '2016',
       image: 'https://lh3.googleusercontent.com/QOGK56A1wOY7nlwUIimHQ3zM1FUhw5R_6gmemx76iF1foO-Bbx1VY6NO0GmSVtVEc7iVNqZ1mGoQ28Nu=w544-h544-l90-rj',
-      url: 'https://music.youtube.com/watch?playlist=OLAK5uy_lzgyyhyxs__3yDSnPyq46LNpFmrDUfSQ4'
+      url: 'https://music.youtube.com/watch?list=OLAK5uy_lzgyyhyxs__3yDSnPyq46LNpFmrDUfSQ4'
   }
 ];
