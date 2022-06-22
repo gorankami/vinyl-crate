8de1d370901836fd327e0139ec4784514981a5db
Goran <gorankami@gmail.com>
2022-06-22 10:23:22 +0000

    Added shuffle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Album from "./Album";
 import { data } from "./data";
+import { fisherYatesShuffle } from "./shuffleArray";
+
 export default function App() {
+  const shuffledData = fisherYatesShuffle(data);
   return (
     <div className="container pt-4 bg-dark">
       <div className="row ">
-        {data.map((item, i) => (
+        {shuffledData.map((item, i) => (
           <Album key={i} item={item} />
         ))}
       </div>
